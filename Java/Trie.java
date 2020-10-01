c49e931a2b4d7c8ef737683c1df8c5233e1c0dea
Tirthankar Bhattacharya <shubho2001g@gmail.com>
2020-10-01 18:54:04 +0530

    My implementation of the trie data structure in java

---
+++ Java/Trie.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.util.*;
+class TrieNode
+{
+    char value;
+    boolean end;
+    HashMap<Character, TrieNode>children;
+    String word;
+    TrieNode(char ch)
+    {
+        this.value = ch;
+        this.children = new HashMap<>();
+        this.end=false;
+        this.word = null;
+    }
+}
+class Trie {
+
+    /** Initialize your data structure here. */
+    TrieNode head;
+    public Trie() {
+        this.head = new TrieNode('*');
+    }
+
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        TrieNode current = this.head;
+        for (int pos = 0; pos < word.length(); pos++) {
+            char ch = word.charAt(pos);
+            if (!current.children.containsKey(ch))
+                current.children.put(ch,new TrieNode(ch));
+            current = current.children.get(ch);
+        }
+        current.end = true;
+        current.word = word;
+    }
+
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        boolean exists = true;
+        TrieNode current = this.head;
+        for (int pos = 0; pos < word.length(); pos++) {
+            char ch = word.charAt(pos);
+            if (!current.children.containsKey(ch))
+            {
+                exists = false;
+                break;
+            }
+            current = current.children.get(ch);
+        }
+        if (!exists)
+            return false;
+        else
+            return current.end;
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        boolean exists = true;
+        TrieNode current = this.head;
+        for (int pos = 0; pos < prefix.length(); pos++) {
+            char ch = prefix.charAt(pos);
+            if (!current.children.containsKey(ch))
+            {
+                exists = false;
+                break;
+            }
+            current = current.children.get(ch);
+        }
+
+        return exists;
+    }
+}
